275c267cf3f866db2f85e36f32680d879376a941
Jay Two <j2doll@gmail.com>
2018-12-30 21:39:20 +0900

    initial commit

---
+++ hello/hello.js
@@ -0,0 +1,26 @@
+// hello.js 
+// code from https://graphql.org/graphql-js/
+
+// npm i graphql
+var { graphql, buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
+
+// console output is { data: { hello: 'Hello world!' } }
